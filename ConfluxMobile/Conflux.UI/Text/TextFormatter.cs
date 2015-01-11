c3777da8ce352a2d788615dedf5f56876508f693
Vasile Falamas <vasile.falamas@hotmail.com>
2015-01-12 00:49:11 +0200

    Description in EventDetails page is now formatted so that possible hyperlinks (emails, URLs) are clickable and trigger navigation.

---
+++ ConfluxMobile/Conflux.UI/Text/TextFormatter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Windows.UI.Xaml.Documents;
+
+namespace Conflux.UI.Text
+{
+    public class TextFormatter
+    {
+        private const string Pattern = @"((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)";
+
+        public IList<Inline> FormatToRichText(string plainText)
+        {
+            var runsResult = new List<Inline>();
+
+            int index = 0;
+
+            Regex regex = new Regex(Pattern);
+             
+            var matches = regex.Matches(plainText);
+
+            foreach (Match match in matches)
+            {
+                //Add a regular text match
+                int matchIndex = match.Index;
+                var text = plainText.Substring(index, matchIndex - index);
+
+                var textRun = new Run
+                {
+                    Text = text
+                };
+                runsResult.Add(textRun);
+                
+                //Add a hyperlink match
+                var hyperlinkText = match.Value;
+                var hyperlinkRun = new Run
+                {
+                    Text = hyperlinkText
+                };
+
+                var hyperlinkNavigateUri = hyperlinkText;
+
+                if (!hyperlinkNavigateUri.Contains("@") && !hyperlinkNavigateUri.StartsWith("http"))
+                {
+                    hyperlinkNavigateUri = @"http://" + hyperlinkNavigateUri;
+                }
+
+                if (hyperlinkNavigateUri.Contains("@") && !hyperlinkNavigateUri.StartsWith("mailto"))
+                {
+                    hyperlinkNavigateUri = @"mailto://" + hyperlinkNavigateUri;
+                }
+
+                var hyperlink = new Hyperlink
+                {
+                    NavigateUri = new Uri(hyperlinkNavigateUri)
+                };
+                hyperlink.Inlines.Add(hyperlinkRun);
+
+                runsResult.Add(hyperlink);
+                
+                index = matchIndex + match.Length;
+            }
+
+            var lastRegularText = plainText.Substring(index, plainText.Length - index);
+            runsResult.Add(new Run { Text = lastRegularText });
+
+            return runsResult;
+        }
+    }
+}
---
+++ ConfluxMobile/Conflux.UI/TextBlockExtension.cs
@@ -0,0 +1,41 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using TextFormatter = Conflux.UI.Text.TextFormatter;
+
+namespace Conflux.UI
+{
+    public static class TextBlockExtension
+    {
+        public static readonly DependencyProperty FormattedTextProperty =
+            DependencyProperty.Register("FormattedText", typeof(string), typeof(TextBlockExtension), new PropertyMetadata(string.Empty,
+                (sender, e) =>
+                {
+                    var text = e.NewValue as string;
+                    var textBlock = (sender) as TextBlock;
+
+                    if (textBlock != null)
+                    {
+                        textBlock.Inlines.Clear();
+
+                        var formatter = new TextFormatter();
+
+                        var inlines = formatter.FormatToRichText(text);
+
+                        foreach (var inline in inlines)
+                        {
+                            textBlock.Inlines.Add(inline);
+                        }
+                    }
+                }));
+
+        public static string GetFormattedText(DependencyObject dependencyObject)
+        {
+            return (string)dependencyObject.GetValue(FormattedTextProperty);
+        }
+
+        public static void SetFormattedText(DependencyObject dependencyObject, string value)
+        {
+            dependencyObject.SetValue(FormattedTextProperty, value);
+        }
+    }
+}
