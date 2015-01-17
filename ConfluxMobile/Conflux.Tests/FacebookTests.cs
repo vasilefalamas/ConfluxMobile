b48373d3741dede80828d8b80c2804049af9c2da
Vasile Falamas <vasile.falamas@hotmail.com>
2015-01-17 20:08:43 +0200

    Added blank tests project.

---
+++ ConfluxMobile/Conflux.Tests/FacebookTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Conflux.Tests
+{
+    [TestClass]
+    public class FacebookTests
+    {
+        [TestMethod]
+        public void FacebookTests1()
+        {
+        }
+    }
+}
