2707c24611ccd7664974f427e89744b4e056700f
Vasile Falamas <vasile.falamas@hotmail.com>
2015-03-19 00:01:00 +0200

    Removed Tests project.

--- ConfluxMobile/Conflux.Tests/Properties/AssemblyInfo.cs
+++ ConfluxMobile/Conflux.Tests/Properties/AssemblyInfo.cs
@@ -14,14 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0c51116e-9514-4382-8b6f-f38f760d5d2a")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -34,3 +26,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
