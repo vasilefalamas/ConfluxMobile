b443219e92b29df57e66c7197ac15a81ab03496a
Vasile Falamas <vasile.falamas@hotmail.com>
2015-03-25 00:50:53 +0200

    Added test project.

---
+++ ConfluxMobile/Conflux.Tests/FacebookProviderMock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Conflux.Connectivity.Authentication;
+using Conflux.Connectivity.GraphApi;
+
+namespace Conflux.Tests
+{
+    public class FacebookProviderMock : IFacebookProvider
+    {
+        public Task<string> GetUserNameInfoAsync(AccessToken accessToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetLocationInfoAsync(long locationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AuthenticationResult GetAuthenticationResult(string authenticationResponse)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetProfilePictureAsync(AccessToken accessToken, PictureSize pictureSize = PictureSize.Size160x160)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetEventsByKeywordAsync(AccessToken accessToken, string locationKeyword, uint offset = 0, uint? limit = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetEventAsync(AccessToken accessToken, string eventId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetMyEvents(AccessToken accessToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
