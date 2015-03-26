9e58a8b48a9ff6bf557015d4ef09c68510a0e55c
Vasile Falamas <vasile.falamas@hotmail.com>
2015-03-26 02:46:00 +0200

    Added FacebookProviderTests test class with initial test.

---
+++ ConfluxMobile/Conflux.Tests/FacebookProviderTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Conflux.Connectivity;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Conflux.Tests
+{
+    [TestClass]
+    public class FacebookProviderTests
+    {
+        [TestMethod]
+        public async Task WhenUserDataRetrieved_NamePopulated()
+        {
+            
+             FacebookDataAccess fbDataAccess;
+
+             var fbProviderMock = new FacebookProviderMock();
+             fbDataAccess = new FacebookDataAccess(fbProviderMock);
+
+            var userInfo = await fbDataAccess.GetUserNameInfoAsync(null);
+
+            Assert.AreEqual(userInfo.FirstName, "John");
+            Assert.AreEqual(userInfo.LastName, "Doe");
+
+        }
+    }
+}
