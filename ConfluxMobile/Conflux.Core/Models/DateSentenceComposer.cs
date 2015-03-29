55da51eef16ee1cdca311bb42c1973b78d06c0ed
Vasile Falamas <vasile.falamas@hotmail.com>
2015-03-29 04:22:14 +0300

    Added DateSentenceComposer to transform DateTime values into friendly time sentences.

---
+++ ConfluxMobile/Conflux.Core/Models/DateSentenceComposer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Conflux.Core.Extensions;
+
+namespace Conflux.Core.Models
+{
+    public class DateSentenceComposer
+    {
+        public static string ComposeBeginSentence(DateTime? startDate)
+        {
+            if (!startDate.HasValue)
+            {
+                return null;
+            }
+
+            var startDateValue = startDate.Value;
+
+            if (startDateValue.IsYesterday())
+            {
+                return string.Format("Has started yesterday.");
+            }
+
+            return string.Format("Started on {0}.", startDateValue);
+        }
+
+        public static string ComposeEndSentence(DateTime? endDate)
+        {
+            if (!endDate.HasValue)
+            {
+                return null;
+            }
+
+            return string.Format("Ended on {0}.", endDate.Value);
+        }
+    }
+}
