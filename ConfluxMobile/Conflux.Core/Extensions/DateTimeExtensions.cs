a93ce0deec5de567a186d81a3d791abaa835f47d
Vasile Falamas <vasile.falamas@hotmail.com>
2015-03-29 20:23:17 +0300

    Refactoring on DateTime Extensions.

--- ConfluxMobile/Conflux.Core/Extensions/DateTimeExtensions.cs
+++ ConfluxMobile/Conflux.Core/Extensions/DateTimeExtensions.cs
@@ -6,31 +6,26 @@ namespace Conflux.Core.Extensions
     {
         public static bool IsYesterday(this DateTime dateTime)
         {
-            var supposedYesterday = GetTrimmedDateTime(dateTime);
-            var today = GetTrimmedDateTime(DateTime.Now);
+            var supposedYesterday = dateTime.Date;
+            var today = DateTime.Now.Date;
 
             return today.Equals(supposedYesterday.AddDays(1));
         }
 
         public static bool IsToday(this DateTime dateTime)
         {
-            var supposedToday = GetTrimmedDateTime(dateTime);
-            var today = GetTrimmedDateTime(DateTime.Now);
+            var supposedToday = dateTime.Date;
+            var today = DateTime.Now.Date;
             
             return today.Equals(supposedToday);
         }
 
         public static bool IsTomorrow(this DateTime dateTime)
         {
-            var supposedTomorrow = GetTrimmedDateTime(dateTime);
-            var today = GetTrimmedDateTime(DateTime.Now);
+            var supposedTomorrow = dateTime.Date;
+            var today = DateTime.Now.Date;
 
             return today.AddDays(1).Equals(supposedTomorrow);
-        }
-
-        private static DateTime GetTrimmedDateTime(DateTime dateTime)
-        {
-            return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day);
         }
     }
 }
