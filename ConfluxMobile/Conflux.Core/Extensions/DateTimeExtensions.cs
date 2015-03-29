67c41b15bc05113ec9b6626b8ed5ddbd0f36cb5d
Vasile Falamas <vasile.falamas@hotmail.com>
2015-03-29 04:21:17 +0300

    Added IsYesterday, IsToday, IsTomorrow DateTime extension methods.

---
+++ ConfluxMobile/Conflux.Core/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Conflux.Core.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static bool IsYesterday(this DateTime dateTime)
+        {
+            var currentDate = DateTime.Now;
+
+            var today = GetTrimmedDateTime(currentDate);
+            var lessPreciseDate = GetTrimmedDateTime(dateTime);
+
+            return today.Equals(lessPreciseDate.AddDays(1));
+        }
+
+        public static bool IsToday(this DateTime dateTime)
+        {
+            var currentDate = DateTime.Now;
+
+            var today = GetTrimmedDateTime(currentDate);
+            var lessPreciseDate = GetTrimmedDateTime(dateTime);
+
+            return today.Equals(lessPreciseDate);
+        }
+
+        public static bool IsTomorrow(this DateTime dateTime)
+        {
+            var currentDate = DateTime.Now;
+
+            var today = GetTrimmedDateTime(currentDate);
+            var lessPreciseDate = GetTrimmedDateTime(dateTime);
+
+            return today.AddDays(1).Equals(lessPreciseDate);
+        }
+
+        private static DateTime GetTrimmedDateTime(DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day);
+        }
+    }
+}
