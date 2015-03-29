e0d3fc1575c42d000c066b32de3e48150233d71a
Vasile Falamas <vasile.falamas@hotmail.com>
2015-03-29 06:23:10 +0300

    Added XAML DataSentenceConverter.

---
+++ ConfluxMobile/Conflux.UI/Converters/DateSentenceConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+using Conflux.Core.Models;
+
+namespace Conflux.UI.Converters
+{
+    public class DateSentenceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var dateTime = value as DateTime?;
+
+            if (!dateTime.HasValue)
+            {
+                return null;
+            }
+
+            if (parameter.Equals("Start"))
+            {
+                return DateSentenceComposer.ComposeBeginSentence(dateTime.Value);
+            }
+
+            if (parameter.Equals("End"))
+            {
+                return DateSentenceComposer.ComposeEndSentence(dateTime.Value);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
