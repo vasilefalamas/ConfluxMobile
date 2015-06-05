f7164a992d6b64a3c24348fcf5e2bf4e93dac50e
Vasile Falamas <vasile.falamas@hotmail.com>
2015-06-05 04:30:57 +0300

    Performed changes according to the new redesign.

--- ConfluxMobile/Conflux.UI/Converters/BoolToColorConverter.cs
+++ ConfluxMobile/Conflux.UI/Converters/BoolToColorConverter.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Windows.UI.Xaml;
+using Windows.UI;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Media;
 
@@ -7,18 +7,18 @@ namespace Conflux.UI.Converters
 {
     public class BoolToColorConverter : IValueConverter
     {
-        private static readonly SolidColorBrush accentColor = Application.Current.Resources["PhoneAccentBrush"] as SolidColorBrush;
+        private static readonly SolidColorBrush ReadEventColor = new SolidColorBrush(Colors.Orange);
 
-        private static readonly SolidColorBrush staticColor = Application.Current.Resources["MidBrush"] as SolidColorBrush;
+        private static readonly SolidColorBrush UnreadEventColor = new SolidColorBrush(Colors.DarkGray);
 
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return ((bool)value) ? staticColor : accentColor;
+            return ((bool)value) ? UnreadEventColor : ReadEventColor;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
-            return (SolidColorBrush)value != accentColor;
+            return (SolidColorBrush)value != ReadEventColor;
         }
     }
 }
