5eab3cbb82df93630a3e0d3ae48b1f17ea8c9af2
Vasile Falamas <vasile.falamas@hotmail.com>
2015-06-21 03:42:38 +0300

    Added interface for FacebookRequesthandler (former FacebookProvider).

---
+++ ConfluxMobile/Conflux.Connectivity/GraphApi/IFacebookRequestHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+namespace Conflux.Connectivity.GraphApi
+{
+    public interface IFacebookRequestHandler
+    {
+        Task<string> GetUserNameInfoAsync();
+
+        Task<string> GetLocationInfoAsync(long locationId);
+        
+        Task<string> GetProfilePictureAsync(PictureSize pictureSize = PictureSize.Size160x160);
+
+        Task<string> GetEventsByKeywordAsync(string locationKeyword, uint offset = 0, uint? limit = null);
+
+        Task<string> GetEventAsync(string eventId);
+
+        Task<string> GetMyEvents();
+
+        Task<string> PostEventAttendance(string eventId);
+    }
+}
