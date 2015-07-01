77d363c6f153ca9e5671b8eb616f949c03dc57f5
Vasile Falamas <vasile.falamas@hotmail.com>
2015-07-01 21:51:17 +0300

    Highlight events can now be tapped for additional details.

--- ConfluxMobile/Conflux.UI/Controls/HighlightEvents.xaml.cs
+++ ConfluxMobile/Conflux.UI/Controls/HighlightEvents.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Conflux.Core.Models;
 using Conflux.UI.ViewModels;
+using Conflux.UI.Views;
 
 namespace Conflux.UI.Controls
 {
@@ -21,6 +24,28 @@ namespace Conflux.UI.Controls
             if (viewModel.EventsGroups.Count == 0) //OneTime load of highlights on a cached page.
             {
                 await viewModel.GetHighlights();
+            }
+        }
+
+        private void OnHighlightsEventsListSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listView = sender as ListView;
+
+            if (listView == null)
+            {
+                return;
+            }
+            
+            var selectedItem = (EventDisplayItem)listView.SelectedItem;
+
+            if (selectedItem != null)
+            {
+                selectedItem.Visited = true;
+
+                var frame = (Frame) Window.Current.Content;
+                frame.Navigate(typeof(EventDetails), selectedItem);
+
+                listView.SelectedItem = null;
             }
         }
     }
